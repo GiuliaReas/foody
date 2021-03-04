b041401ef1d14e8f76b8b521e1cb21310f88d1be
Giulia Reas <56250206+giuliaReas@users.noreply.github.com>
2021-03-04 18:55:57 +0100

    autocomplete improved

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -5,25 +5,29 @@ import {
   switchMap,
 } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Iproduct } from './../../models/product';
 
 import { SearchService } from './../../services/search.service';
+import { Subscription } from 'rxjs';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
   products: Iproduct[];
   myControl = new FormControl();
+  subscription: Subscription;
+  selectedProduct: Iproduct;
 
   constructor(private service: SearchService) {}
 
   ngOnInit(): void {
-    this.myControl.valueChanges
+    this.subscription = this.myControl.valueChanges
       .pipe(
         filter((res) => res.length > 1),
         debounceTime(300),
@@ -31,5 +35,13 @@ export class SearchComponent implements OnInit {
         switchMap((text) => this.service.getProtucts(text))
       )
       .subscribe((result) => (this.products = result));
+  }
+
+  onSelectionChanged($event: MatAutocompleteSelectedEvent): Iproduct {
+    return (this.selectedProduct = $event?.option?.value);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
